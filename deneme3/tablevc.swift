3d4ef838dae0c50eac0969f827dc002d1ef37c7b
erdemoden <erdemoden5@gmail.com>
2021-04-05 22:20:49 +0300

    first commit

---
+++ deneme3/tablevc.swift
@@ -0,0 +1,30 @@
+//
+//  tablevc.swift
+//  deneme3
+//
+//  Created by erdem öden on 23.02.2021.
+//  Copyright © 2021 erdem öden. All rights reserved.
+//
+
+import UIKit
+
+class tablevc: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
